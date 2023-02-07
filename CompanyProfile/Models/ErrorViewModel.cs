59a00dd7ceed157ea10b1b4b78f5e06e37a63c06
rashadmustafa2244 <rashadmustafa2244@outlook.com>
2023-02-07 16:10:15 +0300

    Add project files.

---
+++ CompanyProfile/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CompanyProfile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
